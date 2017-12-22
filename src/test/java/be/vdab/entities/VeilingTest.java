21cdbf63cd81a9fc041b03e9c14816356227285e
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2017-12-22 15:22:27 +0100

    Oefening 3: Test fixtures

--- src/test/java/be/vdab/entities/VeilingTest.java
+++ src/test/java/be/vdab/entities/VeilingTest.java
@@ -1,31 +1,34 @@
 package be.vdab.entities;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class VeilingTest {
-
+	private Veiling veiling;
+	
+	@Before
+	public void before() {
+		veiling = new Veiling();
+	}
+	
 	@Test
 	public void alsNogGeenBodUitgevoerdWerdIsHoogsteBodNul() {
-		Veiling veiling = new Veiling();
 		assertEquals(0, veiling.getHoogstebod());
 	}
 	
 	@Test
 	public void alsEersteBodUitgevoerdWerdIsHoogsteBodEersteBod() {
-		Veiling veiling = new Veiling();
 		veiling.doeBod(1000);
 		assertEquals(1000, veiling.getHoogstebod());
 	}
 	
 	@Test
 	public void alsMeerdereBodenUitgevoerdWerdenIsHoogsteBodGelijkAanBedragHoogsteBod() {
-		Veiling veiling = new Veiling();
 		veiling.doeBod(1000);
 		veiling.doeBod(2000);
 		veiling.doeBod(1500);
 		assertEquals(2000, veiling.getHoogstebod());
 	}
-
 }
