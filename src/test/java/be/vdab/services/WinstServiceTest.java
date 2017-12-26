5f54d12db2b9ffe6b452953ef462a962f656c4ac
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2017-12-26 16:54:46 +0100

    Oefening 6: Mock

--- src/test/java/be/vdab/services/WinstServiceTest.java
+++ src/test/java/be/vdab/services/WinstServiceTest.java
@@ -1,6 +1,9 @@
 package be.vdab.services;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 
@@ -8,17 +11,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 import be.vdab.repositories.KostRepository;
-import be.vdab.repositories.KostRepositoryStub;
 import be.vdab.repositories.OpbrengstRepository;
-import be.vdab.repositories.OpbrengstRepositoryStub;
 
 public class WinstServiceTest {
 	private WinstService winstService;
+	private OpbrengstRepository opbrengstRepository;
+	private KostRepository kostRepository;
 	
 	@Before
 	public void before() {
-		OpbrengstRepository opbrengstRepository = new OpbrengstRepositoryStub();
-		KostRepository kostRepository = new KostRepositoryStub();
+		opbrengstRepository = mock(OpbrengstRepository.class);
+		kostRepository = mock(KostRepository.class);
+		when(opbrengstRepository.findTotaleOpbrengst())
+			.thenReturn(BigDecimal.valueOf(5000));
+		when(kostRepository.findTotaleKost())
+			.thenReturn(BigDecimal.valueOf(2500));
 		winstService = new WinstService(opbrengstRepository,
 			kostRepository);		
 	}
@@ -27,6 +34,8 @@ public class WinstServiceTest {
 	public void getWinstTest() {
 		assertEquals(0, BigDecimal.valueOf(2500.00).compareTo(
 			winstService.getWinst()));
+		verify(opbrengstRepository).findTotaleOpbrengst();
+		verify(kostRepository).findTotaleKost();
 	}
 
 }
